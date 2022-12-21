4d1cb2ff450afc68070b53833f4c36c2d3702ae7
Jannik Keye <jannik.keye@gmail.com>
2022-12-21 12:43:33 +0100

    fix: calculations

--- islands/Calculator.tsx
+++ islands/Calculator.tsx
@@ -16,6 +16,45 @@ const formatNumber = (
   return temp.toFixed(2) + (opts.percent ? "%" : "");
 };
 
+const modelBalanced = (a: number, b: number, f: number) => {
+  const diff = (a * f - b * f) / (2 * ((a * f + b * f) / 2));
+
+  const fixA = f / 2 + (f / 2) * diff;
+  const fixB = f / 2 - (f / 2) * diff;
+
+  return {
+    diff,
+    fixA,
+    fixB,
+  };
+};
+
+const modelMin0 = (a: number, b: number, f: number) => {
+  const diff = Math.max(-1, Math.min(1, (a / b - b / a) * 0.25));
+
+  const fixA = Math.max(0, f / 2 + (f / 2) * diff);
+  const fixB = Math.min(f, f / 2 - (f / 2) * diff);
+
+  return {
+    diff,
+    fixA,
+    fixB,
+  };
+};
+
+const modelStandard = (a: number, b: number, f: number) => {
+  const diff = (a / b - b / a) * 0.25;
+
+  const fixA = f / 2 + (f / 2) * diff;
+  const fixB = f / 2 - (f / 2) * diff;
+
+  return {
+    diff,
+    fixA,
+    fixB,
+  };
+};
+
 export const Calculator = () => {
   const [fixCosts, setFixCosts] = useState(150);
   const [salaries, setSalaries] = useState<{ a: number; b: number }>({
@@ -32,13 +71,30 @@ export const Calculator = () => {
     fixB: [0, ""],
   });
 
+  const [calculationFn, setCalculationFn] = useState("standard");
+
   useEffect(() => {
     const { a, b } = salaries;
 
-    const diff = a > 0 || b > 0 ? (a / b - b / a) * 0.25 : 0;
+    let fn: typeof modelStandard = modelStandard;
 
-    const fixA = fixCosts / 2 + (fixCosts / 2) * diff;
-    const fixB = fixCosts / 2 - (fixCosts / 2) * diff;
+    switch (calculationFn) {
+      case "standard": {
+        fn = modelStandard;
+
+        break;
+      }
+      case "balanced": {
+        fn = modelBalanced;
+
+        break;
+      }
+      case "minmaxed": {
+        fn = modelMin0;
+      }
+    }
+
+    const { diff, fixA, fixB } = fn(a, b, fixCosts);
 
     setResult({
       diff: [diff, `(${a} / ${b} - ${b} / ${a}) * 0.25`],
@@ -51,7 +107,7 @@ export const Calculator = () => {
         `${fixCosts} / 2 - (${fixCosts} / 2) * ${diff.toPrecision(2)}`,
       ],
     });
-  }, [salaries, fixCosts]);
+  }, [salaries, fixCosts, calculationFn]);
 
   const labels = [
     "FC/2",
@@ -67,6 +123,21 @@ export const Calculator = () => {
 
   return (
     <div>
+      <div class="w-full flex justify-between">
+        <label>Berechnung:</label>
+        <select
+          onChange={(e) => {
+            const value = e.target?.value;
+
+            setCalculationFn(value);
+          }}
+        >
+          <option value="standard">Ausgleichend</option>
+          <option value="balanced">Balancierend</option>
+          <option value="minmaxed">Keine Negativen</option>
+        </select>
+      </div>
+      <hr class="my-4" />
       <div class="flex flex-1 flex-col mb-4">
         <label htmlFor="salaryA">A1</label>
         <Input
@@ -157,11 +228,11 @@ export const Calculator = () => {
       <br />
 
       <div class="text-blue-500 text-xs flex flex-col gap-2">
-        <h3 class="text-underline mb-2">Help</h3>
+        <h3 class="text-underline mb-2">Hilfe</h3>
         <p>A1 = Available funds for A</p>
         <p>B1 = Available funds for B</p>
         <p>Fix Costs (FC) = Total costs</p>
-        <p>50/50 = Total Fix Costs divided by 2</p>
+        <p>FC/2 = Total Fix Costs divided by 2</p>
         <p>A2 = Pro rata cost of A</p>
         <p>B2 = Pro rata cost of B</p>
         <p>Abs. Diff = Total difference between pro ratas</p>
