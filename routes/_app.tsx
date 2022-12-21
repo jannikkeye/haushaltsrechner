37dbe384b777ccc94b09e14b0da1db554ccd8af4
Jannik Keye <jannik.keye@gmail.com>
2022-12-21 10:53:59 +0100

    fix: title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
-        <title>Calculator 🐑❤️🦆</title>
+        <title>Calculator</title>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🦆</text></svg>"
